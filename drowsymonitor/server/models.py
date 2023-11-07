2217644d105367267c29e9bf4c8e4c38c87e9981
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-11-07 16:28:43 +0700

    add vehicle model to server folder

--- drowsymonitor/server/models.py
+++ drowsymonitor/server/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.conf import settings
+
+
+class Vehicle(models.Model):
+    vehicle_model = models.CharField(max_length=100)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="vehicle_owner"
+    )
+    driver_name = models.CharField(max_length=100)
+    plate_licence = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
