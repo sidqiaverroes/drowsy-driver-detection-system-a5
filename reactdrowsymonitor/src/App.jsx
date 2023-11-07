a6e60f817b1e5ac41cce9e2abc7be9f7e94936e9
sidqiaverroes <sidqi.averroes26@gmail.com>
2023-11-07 14:10:31 +0700

    initial frontend files

---
+++ reactdrowsymonitor/src/App.jsx
@@ -0,0 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
